f663effa33c68c9906c5d95424fba166827d2d07
陈伟 <templechen>
2021-08-11 14:46:18 +0800

    temp commit

--- app/src/main/java/com/temple/shaderimage/MainActivity.kt
+++ app/src/main/java/com/temple/shaderimage/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.temple.shaderimage
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.res.ResourcesCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,25 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val options = BitmapFactory.Options().apply {
-            inPreferredConfig = Bitmap.Config.ARGB_8888
-        }
-        val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher, options)
-
-//        val bitmap = Bitmap.createBitmap(144, 144, Bitmap.Config.ARGB_8888)
-////        val bitmap = Bitmap.createBitmap(144, 144, Bitmap.Config.ARGB_4444)
-//
-//        //todo gl那边glReadPixels暂时只支持rgb和rgba，如果有其他类型，需要用libyuv去转换格式，因为此实现为精简模式，就不做这部分了
-////        val bitmap = Bitmap.createBitmap(144, 144, Bitmap.Config.RGB_565)
-////        val bitmap = Bitmap.createBitmap(144, 144, Bitmap.Config.ALPHA_8)
-//        val canvas = Canvas(bitmap)
-//        canvas.drawARGB(255, 255, 255, 255)
-//        val drawable = ResourcesCompat.getDrawable(resources, R.mipmap.ic_launcher, null)
-//        drawable?.setBounds(0, 0, canvas.width, canvas.height)
-//        drawable?.draw(canvas)
-        shaderImageView.setImageBitmap(bitmap)
-        shaderImageView.setShaderType(3)
 
         button.setOnClickListener {
             startActivity(Intent(this, ListTestActivity::class.java))
